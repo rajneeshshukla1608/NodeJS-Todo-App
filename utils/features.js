95c89f441f05ba5126487ec83265285eb1cefe8a
Rajneesh Kumar Shukla <65530868+rajneeshshukla1608@users.noreply.github.com>
2023-05-20 17:33:03 +0530

    Update features.js

--- utils/features.js
+++ utils/features.js
@@ -1,17 +1,17 @@
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
-export const sendCookie = (user, res, message, statuscode = 201)=>{
-    const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET);
-    res
-    .status(statuscode)
+export const sendCookie = (user, res, message, statusCode = 200) => {
+  const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+  res
+    .status(statusCode)
     .cookie("token", token, {
-        httpOnly : true,
-        maxAge : 15 * 5 * 1000,
-        secure : process.env.NODE_ENV === "Development" ? false : true,
-        sameSite : process.env.NODE_ENV === "Development" ? "lax" :"none",
+      httpOnly: true,
+      maxAge: 15 * 60 * 1000,
+      sameSite: process.env.NODE_ENV === "Development" ? "lax" : "none",
+      secure: process.env.NODE_ENV === "Development" ? false : true,
     })
     .json({
-        success: true,
-        message
+      success: true,
+      message,
     });
 };
